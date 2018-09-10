96e97a60d68ca3277c3da747108fc40063eea45c
Amanze Ogbonna <darlingtonamz@gmail.com>
2018-09-10 10:55:29 +0000

    Added Access-Control-Allow-Origin to allow https amz-sol-frontend.herokuapp.com sites

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost*');
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Origin', 'https://*.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://amz-sol-frontend.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods',
